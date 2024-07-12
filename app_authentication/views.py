c42b94e2e03df9714a265527afbbe616f35742a9
Zwogli <mathiaskohler@web.de>
2024-07-12 09:59:25 +0200

    Create view with serializers

--- app_authentication/views.py
+++ app_authentication/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth import get_user_model
+from .serializers import UserSerializer
+
+
+CustomUser = get_user_model()
+
 
 # Create your views here.
+class UserCreateView(generics.CreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
