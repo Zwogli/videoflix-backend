9339cda48aa3c76903cb5ba9a6e062374f9fd4a0
Zwogli <mathiaskohler@web.de>
2024-07-12 09:42:39 +0200

    Edit correlation viedos_local -

--- app_videoflix/models/videos_local.py
+++ app_videoflix/models/videos_local.py
@@ -1,12 +1,12 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
 
 class LocalVideo(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     upload_date = models.DateTimeField(auto_now_add=True)
     file = models.FileField(upload_to='local_videos/')
-    uploaded_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    uploaded_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
