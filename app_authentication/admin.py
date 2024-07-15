f7eea86e7ef3eae6d0a5b2f60aea18987627180f
Zwogli <mathiaskohler@web.de>
2024-07-15 10:49:13 +0200

    Edit admin dashboard

--- app_authentication/admin.py
+++ app_authentication/admin.py
@@ -5,17 +5,17 @@ from .models import CustomUser
 # Register your models here.
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-    list_display = ('email', 'first_name', 'last_name', 'is_staff', 'is_active')
+    list_display = ('email', 'user_name', 'first_name', 'last_name', 'is_staff', 'is_active', 'is_verified')
     list_filter = ('is_staff', 'is_active')
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
         ('Personal info', {'fields': ('first_name', 'last_name')}),
-        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_verified', 'is_superuser', 'groups', 'user_permissions')}),
     )
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active')}
+            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active', 'is_verified')}
         ),
     )
     search_fields = ('email',)
