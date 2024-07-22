40034ecfa7dfe20e7879951c42b6efdcbc1febda
Zwogli <mathiaskohler@web.de>
2024-07-22 09:05:21 +0200

    Fix url

--- app_authentication/urls.py
+++ app_authentication/urls.py
@@ -4,5 +4,5 @@ from .views import UserCreateView, verify_email, reset_password_with_email
 urlpatterns = [
     path('registration/', UserCreateView.as_view(), name='user-create'),
     path('verify/<uidb64>/<token>/', verify_email, name='verify-email'),
-    path('auth/send-reset-email/', reset_password_with_email, name='send-reset-email'),
+    path('send-reset-email/', reset_password_with_email, name='send-reset-email'),
 ]
