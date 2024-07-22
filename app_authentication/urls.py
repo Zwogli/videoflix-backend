3868bebd76a5b780998a5e820782427eaebb3bc4
Zwogli <mathiaskohler@web.de>
2024-07-22 08:54:20 +0200

    Update urls

--- app_authentication/urls.py
+++ app_authentication/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import UserCreateView, verify_email
+from .views import UserCreateView, verify_email, reset_password_with_email
 
 urlpatterns = [
     path('registration/', UserCreateView.as_view(), name='user-create'),
     path('verify/<uidb64>/<token>/', verify_email, name='verify-email'),
+    path('auth/send-reset-email/', reset_password_with_email, name='send-reset-email'),
 ]
