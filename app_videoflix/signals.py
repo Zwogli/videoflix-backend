2374845e6c81c7d2361decd840f67dcfe5bff6fc
Zwogli <mathiaskohler@web.de>
2024-07-30 10:08:21 +0200

    Create delete video file

--- app_videoflix/signals.py
+++ app_videoflix/signals.py
@@ -1,21 +1,44 @@
+import os
 from django.dispatch import receiver
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 from .models import GlobalVideo, LocalVideo
 
 
 @receiver(post_save, sender=GlobalVideo)
 def video_global_post_save(sender, instance, created, **kwargs):
-    print('Global Video wurde gespeichert.')
     if created:
         print('New global video created.')
     else:
         print('Global video updated.')
+        
+        
+@receiver(post_delete, sender=GlobalVideo)
+def video_global_post_delete(sender, instance, **kwargs):
+    """
+    Deletes file from filessystem
+    when corresponding `GlobalVideo` object is deleted
+    """
+    if instance.file:
+        if os.path.isfile(instance.file.path):
+            os.remove(instance.file.path)
+            print('Global-video is deleted.')
 
 
 @receiver(post_save, sender=LocalVideo)
 def video_local_post_save(sender, instance, created, **kwargs):
-    print('Local Video wurde gespeichert.')
     if created:
         print('New local video created.')
     else:
         print('Local video updated.')
+        
+        
+@receiver(post_delete, sender=LocalVideo)
+def video_global_post_delete(sender, instance, **kwargs):
+    """
+    Deletes file from filessystem
+    when corresponding `LocalVideo` object is deleted
+    """
+    if instance.file:
+        if os.path.isfile(instance.file.path):
+            os.remove(instance.file.path)
+            print('Local-video is deleted.')
