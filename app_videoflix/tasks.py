3e36705a54c9bbefed0c115a3c243bbf14858c94
Zwogli <mathiaskohler@web.de>
2024-08-02 11:08:00 +0200

    Fix convert 720p

--- app_videoflix/tasks.py
+++ app_videoflix/tasks.py
@@ -17,7 +17,7 @@ def convert_480p(source, file_name):
     
 def convert_720p(source, file_name):
     target = file_name[0] + '_720p.mp4'
-    cmd = 'ffmpeg -i "{}" -s hd7200 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, target)    #{} sorce is set as a variable by the method .format(source, target)
+    cmd = 'ffmpeg -i "{}" -s hd720 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, target)    #{} sorce is set as a variable by the method .format(source, target)
     subprocess.run(cmd)
     
 
