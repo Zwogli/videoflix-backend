2a0f89aebe25f4051c3fc55511d623ab0eaeadde
Zwogli <mathiaskohler@web.de>
2024-08-02 10:14:31 +0200

    Edit media more media size

--- app_videoflix/tasks.py
+++ app_videoflix/tasks.py
@@ -1,8 +1,18 @@
 import subprocess
 
 
-def convert_480p(source):
+def convert(source):
     file_name = source.split('.')
+    convert_480p(source, file_name)
+    convert_720p(source, file_name)
+
+
+def convert_480p(source, file_name):
     target = file_name[0] + '_480p.mp4'
     cmd = 'ffmpeg -i "{}" -s hd480 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, target)    #{} sorce is set as a variable by the method .format(source, target)
+    subprocess.run(cmd)
+    
+def convert_720p(source, file_name):
+    target = file_name[0] + '_720p.mp4'
+    cmd = 'ffmpeg -i "{}" -s hd7200 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, target)    #{} sorce is set as a variable by the method .format(source, target)
     subprocess.run(cmd)
