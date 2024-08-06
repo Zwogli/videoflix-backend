f398e935024fa3b61e260d20559aaf809b8d8e00
Zwogli <mathiaskohler@web.de>
2024-08-06 09:31:56 +0200

    Edit video view endpoint

--- app_videoflix/views.py
+++ app_videoflix/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import GlobalVideo, LocalVideo
 from .serializers import GlobalVideoSerializer, LocalVideoSerializer
+from rest_framework.permissions import IsAuthenticated
 
 
 class GlobalVideoViewSet(viewsets.ReadOnlyModelViewSet):
@@ -10,8 +11,14 @@ class GlobalVideoViewSet(viewsets.ReadOnlyModelViewSet):
 
 
 class LocalVideoViewSet(viewsets.ReadOnlyModelViewSet):
-    # ! Only load videos from user
-    queryset = LocalVideo.objects.all()
     serializer_class = LocalVideoSerializer
+    permission_classes = [IsAuthenticated]  # Ensures that the user must be authenticated to access the videos
+
+    def get_queryset(self):
+        # Only retrieve videos of the user currently logged in
+        user = self.request.user  # Get the currently logged in user
+        if user.is_staff:
+            return LocalVideo.objects.all() 
+        return LocalVideo.objects.filter(uploaded_by=user)
 
 
