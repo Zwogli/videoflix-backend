ca0e07d376cf65ce6cc4089c7dd6e1ea042d0158
Zwogli <mathiaskohler@web.de>
2024-08-06 09:18:07 +0200

    Create video endpoints

--- app_videoflix/views.py
+++ app_videoflix/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import GlobalVideo, LocalVideo
+from .serializers import GlobalVideoSerializer, LocalVideoSerializer
 
-# Create your views here.
+
+class GlobalVideoViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = GlobalVideo.objects.all()
+    serializer_class = GlobalVideoSerializer
+
+
+class LocalVideoViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = LocalVideo.objects.all()
+    serializer_class = LocalVideoSerializer
+
+
