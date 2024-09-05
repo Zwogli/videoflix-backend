0830866f6008e8f779e630840197a1ebc3fccff5
Zwogli <mathiaskohler@web.de>
2024-09-05 13:06:03 +0200

    Update urls premissions

--- videoflix/settings.py
+++ videoflix/settings.py
@@ -230,6 +230,7 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:4200',
     'http://127.0.0.1:8000',    # local backend
+    'https://videoflix.mathias-kohler.de',
 ]
 
 CORS_ORIGIN_WHITELIST = [
