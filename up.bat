8310ba1c34d09ab29729c6479bb1dc3b9615b777
Zwogli <mathiaskohler@web.de>
2024-09-07 13:29:03 +0200

    Update script up.bat automatic deployment on server

--- up.bat
+++ up.bat
@@ -2,4 +2,4 @@ git pull
 git add .
 git commit -m "%*"
 git push
-ssh zwogli\zwogli@35.234.78.80 "cd ./home/mathiaskohler91/projects/videoflix-backend/ && git pull"
+ssh zwogli\zwogli@35.234.78.80 "cd /home/mathiaskohler91/projects/videoflix-backend/ && git pull"
