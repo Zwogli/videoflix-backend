3472b5746d8bdb80ebce113c1de0c344547dd8d0
Zwogli <mathiaskohler@web.de>
2024-09-09 18:06:26 +0200

    Update settings django_rq

--- videoflix/settings.py
+++ videoflix/settings.py
@@ -267,8 +267,8 @@ RQ_QUEUES = {
         'HOST': 'localhost',
         'PORT': 6379,
         'DB': 0,
-        'USERNAME': 'some-user',
-        'PASSWORD': 'foobared',
+        'USERNAME': '', #some-user
+        'PASSWORD': '', #foobared
         'DEFAULT_TIMEOUT': 360,
     },
 }
