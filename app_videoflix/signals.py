76703e6b48c96017049d0942e4d7ed513fda086d
Zwogli <mathiaskohler@web.de>
2024-09-12 10:44:23 +0200

    Test upload

--- app_videoflix/signals.py
+++ app_videoflix/signals.py
@@ -16,7 +16,7 @@ def video_global_post_save(sender, instance, created, **kwargs):
         # queue = django_rq.get_queue('default', autocommit=True)
         # queue.enqueue(convert, video_path)
         
-        convert(video_path)
+        # convert(video_path)
         create_thumbnail(video_path, instance, is_global=True)
     else:
         print('Global video updated.')
