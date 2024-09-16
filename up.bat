31a72e720785d392116a106af8e2c9cb5fd3adb6
Zwogli <mathiaskohler@web.de>
2024-09-16 12:47:34 +0200

    Update restart nginx

--- up.bat
+++ up.bat
@@ -4,4 +4,4 @@ git commit -m "%*"
 git push
 ssh zwogli\zwogli@35.234.78.80 "cd /home/mathiaskohler91/projects/videoflix-backend/ && git pull"
 ssh zwogli\zwogli@35.234.78.80 "sudo supervisorctl restart videoflix_gunicorn"
-ssh zwogli\zwogli@35.234.78.80 "sudo systemctl reload nginx"
+ssh zwogli\zwogli@35.234.78.80 "sudo /usr/sbin/nginx reload nginx"
