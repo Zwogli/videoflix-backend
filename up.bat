499357c2336c8bd3627f1c7ec806a9917473f84b
Zwogli <mathiaskohler@web.de>
2024-09-16 12:45:23 +0200

    Update restart gunicorn

--- up.bat
+++ up.bat
@@ -3,3 +3,5 @@ git add .
 git commit -m "%*"
 git push
 ssh zwogli\zwogli@35.234.78.80 "cd /home/mathiaskohler91/projects/videoflix-backend/ && git pull"
+ssh zwogli\zwogli@35.234.78.80 "sudo supervisorctl restart videoflix_gunicorn"
+ssh zwogli\zwogli@35.234.78.80 "sudo systemctl reload nginx"
