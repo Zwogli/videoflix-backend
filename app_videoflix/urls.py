7f30c71ea380136cb936ce4fb6873a55c30212ef
Zwogli <mathiaskohler@web.de>
2024-09-17 12:10:09 +0200

    Update url for view

--- app_videoflix/urls.py
+++ app_videoflix/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import GlobalVideoViewSet, LocalVideoViewSet, UploadVideoView
+from .views import GlobalVideoViewSet, LocalVideoViewSet, UploadVideoView, thumbnail_status
 
 router = DefaultRouter()    #todo definition
 router.register(r'global-videos', GlobalVideoViewSet, basename='globalvideo')
@@ -9,4 +9,5 @@ router.register(r'local-videos', LocalVideoViewSet, basename='localvideo')
 urlpatterns = [
     path('', include(router.urls)),
     path('upload/', UploadVideoView.as_view(), name='video-upload'),
+    path('thumbnail-status/<int:video_id>/', thumbnail_status, name='thumbnail_status'),
 ]
