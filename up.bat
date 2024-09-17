6bd23b4d417ebdf83005f63ba25f8154309b1e34
Zwogli <mathiaskohler@web.de>
2024-09-17 08:57:34 +0200

    Update restart rq-worker

--- up.bat
+++ up.bat
@@ -4,5 +4,6 @@ git commit -m "%*"
 git push
 ssh zwogli\zwogli@35.234.78.80 "cd /home/mathiaskohler91/projects/videoflix-backend/ && git pull"
 ssh zwogli\zwogli@35.234.78.80 "sudo supervisorctl restart videoflix_gunicorn"
+ssh zwogli\zwogli@35.234.78.80 "sudo supervisorctl restart videoflix-rq-worker"
 ssh zwogli\zwogli@35.234.78.80 "sudo /usr/sbin/nginx -s reload"
 
