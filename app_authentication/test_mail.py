5f5fd7a0b46e0c11c0d01eade376123038e0aa79
Zwogli <mathiaskohler@web.de>
2024-09-19 10:24:27 +0200

    Edit test mail

--- app_authentication/test_mail.py
+++ app_authentication/test_mail.py
@@ -4,3 +4,24 @@ from django.http import HttpResponse
 def send_test_email(request):
     send_mail('Test E-Mail', 'Dies ist eine Test-E-Mail.', 'videoflix@mathias-kohler.de', ['mathiaskohler@mail.de'])
     return HttpResponse('Test E-Mail wurde gesendet.')
+
+from django.core.mail import send_mail
+from django.http import HttpResponse
+from django.conf import settings
+from django.core.mail import send_mail
+from django.core.mail import BadHeaderError
+
+def send_test_email(request):
+    try:
+        send_mail(
+            'Test E-Mail',
+            'Dies ist eine Test-E-Mail.',
+            settings.DEFAULT_FROM_EMAIL,
+            ['mathiaskohler@mail.de'],
+            fail_silently=False,
+        )
+        return HttpResponse('Test E-Mail wurde gesendet.')
+    except BadHeaderError:
+        return HttpResponse('Ungültiger Header gefunden.')
+    except Exception as e:
+        return HttpResponse(f'Fehler beim Senden der E-Mail: {str(e)}')
