c2b5e8f09e8c812de925a7705f6c92b06b54e251
Zwogli <mathiaskohler@web.de>
2024-09-30 10:04:49 +0200

    Refactor and add docstring

--- app_authentication/test_mail.py
+++ app_authentication/test_mail.py
@@ -1,17 +1,13 @@
-from django.core.mail import send_mail
-from django.http import HttpResponse
-
-def send_test_email(request):
-    send_mail('Test E-Mail', 'Dies ist eine Test-E-Mail.', 'videoflix@mathias-kohler.de', ['mathiaskohler@mail.de'])
-    return HttpResponse('Test E-Mail wurde gesendet.')
-
-from django.core.mail import send_mail
-from django.http import HttpResponse
 from django.conf import settings
-from django.core.mail import send_mail
-from django.core.mail import BadHeaderError
+from rest_framework import status
+from rest_framework.response import Response
+from django.core.mail import BadHeaderError, send_mail
+
 
 def send_test_email(request):
+    """
+    Sends a test email to the specified recipient.
+    """
     try:
         send_mail(
             'Test E-Mail',
@@ -20,8 +16,8 @@ def send_test_email(request):
             ['mathiaskohler@mail.de'],
             fail_silently=False,
         )
-        return HttpResponse('Test E-Mail wurde gesendet.')
+        return Response({'message': 'Test email has been sent.'}, status=status.HTTP_200_OK)
     except BadHeaderError:
-        return HttpResponse('Ungültiger Header gefunden.')
+        return Response({'error': 'Invalid header found.'}, status=status.HTTP_400_BAD_REQUEST)
     except Exception as e:
-        return HttpResponse(f'Fehler beim Senden der E-Mail: {str(e)}')
+        return Response({'error': f'Error when sending the e-mail: {str(e)}'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
