835dc61f1552e58bc59ffe0f78c5fa9c909b6930
Zwogli <mathiaskohler@web.de>
2024-10-01 09:10:47 +0200

    Create test models for tests

--- app_videoflix/tests/conftest.py
+++ app_videoflix/tests/conftest.py
@@ -1,6 +1,8 @@
 import pytest
 from django.contrib.auth import get_user_model
-from ..models import GlobalVideo, LocalVideo 
+from django.core.files.uploadedfile import SimpleUploadedFile
+from ..models import GlobalVideo, LocalVideo
+
 
 @pytest.fixture
 def user(db):
@@ -9,16 +11,19 @@ def user(db):
 
 @pytest.fixture
 def global_video(db, user):
+    video_file = SimpleUploadedFile("test_video.mp4", b"file_content", content_type="video/mp4")
     return GlobalVideo.objects.create(
         title='Test Global Video',
-        user=user,
-        # Setze hier weitere erforderliche Felder
+        description='This is a test description for the global video.',
+        file=video_file,  # A mock file is transferred here
     )
 
 @pytest.fixture
 def local_video(db, user):
+    local_video_file = SimpleUploadedFile("test_local_video.mp4", b"file_content", content_type="video/mp4")
     return LocalVideo.objects.create(
         title='Test Local Video',
-        user=user,
-        # Setze hier weitere erforderliche Felder
+        description='This is a test description for the local video.',
+        file=local_video_file,  # A mock file is transferred here
+        uploaded_by=user,
     )
