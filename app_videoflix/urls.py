6ba175e0b5ca49c9a72d779e06ab1f16fd362534
Zwogli <mathiaskohler@web.de>
2024-10-01 08:14:09 +0200

    Refactor code, add comments

--- app_videoflix/urls.py
+++ app_videoflix/urls.py
@@ -2,12 +2,12 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import GlobalVideoViewSet, LocalVideoViewSet, UploadVideoView, thumbnail_status
 
-router = DefaultRouter()    #todo definition
+router = DefaultRouter()
 router.register(r'global-videos', GlobalVideoViewSet, basename='globalvideo')
 router.register(r'local-videos', LocalVideoViewSet, basename='localvideo')
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('upload/', UploadVideoView.as_view(), name='video-upload'),
-    path('thumbnail-status/<int:video_id>/', thumbnail_status, name='thumbnail_status'),
+    path('', include(router.urls)), # Video listing and detail views
+    path('upload/', UploadVideoView.as_view(), name='video-upload'), # Upload a new video
+    path('thumbnail-status/<int:video_id>/', thumbnail_status, name='thumbnail_status'), # Check thumbnail generation status
 ]
