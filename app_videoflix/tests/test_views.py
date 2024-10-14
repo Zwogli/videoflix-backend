2f805c29c28c7a4d3699fadeb216710eec01e6e8
Zwogli <mathiaskohler@web.de>
2024-10-14 08:06:15 +0200

    Create LocalVideoViewSetTests test case's

--- app_videoflix/tests/test_views.py
+++ app_videoflix/tests/test_views.py
@@ -1,7 +1,7 @@
 from django.core.files.uploadedfile import InMemoryUploadedFile
 from rest_framework import status
 from rest_framework.test import APITestCase
-from ..models import GlobalVideo
+from ..models import GlobalVideo, LocalVideo
 from app_authentication.models import CustomUser
 import io
 from django.urls import reverse
@@ -33,3 +33,72 @@ class GlobalVideoViewSetTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 1)
         self.assertEqual(response.data[0]['title'], self.global_video.title)
+        
+        
+class LocalVideoViewSetTests(APITestCase):
+    def setUp(self):
+        self.user = CustomUser.objects.create_user(email='user@mail.com', password='testpassword')
+        self.admin_user = CustomUser.objects.create_superuser(email='admin@mail.com', password='adminpassword')
+
+        self.client.login(email='user@mail.com', password='testpassword')
+
+    def test_create_local_video(self):
+        uploaded_file = create_dummy_file(file_name='local_video.mp4')
+
+        url = reverse('localvideo-list')
+        data = {
+            'title': 'New Local Video',
+            'description': 'Description of new local video',
+            'file': uploaded_file
+        }
+        response = self.client.post(url, data, format='multipart')
+        
+        if response.status_code != status.HTTP_201_CREATED:
+            print('Test respond', response.data)
+        
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(LocalVideo.objects.count(), 1)
+        self.assertEqual(LocalVideo.objects.get().title, 'New Local Video')
+
+    def test_get_local_videos(self):
+        # Erstelle ein lokales Video
+        uploaded_file = create_dummy_file(file_name='local_video.mp4')
+        LocalVideo.objects.create(title='Local Video', description='Test video', uploaded_by=self.user, file=uploaded_file)
+
+        # Teste das Abrufen der lokalen Videos
+        url = reverse('localvideo-list')  # Name für die Listenansicht
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)  # Überprüfe, ob das Video abgerufen wird
+        self.assertEqual(response.data[0]['title'], 'Local Video')
+
+    def test_update_local_video(self):
+        # Erstelle ein lokales Video
+        uploaded_file = create_dummy_file(file_name='local_video.mp4')
+        local_video = LocalVideo.objects.create(title='Old Title', description='Test video', uploaded_by=self.user, file=uploaded_file)
+
+        # Teste das Aktualisieren des lokalen Videos
+        url = reverse('localvideo-detail', args=[local_video.id])  # URL für die Detailansicht
+        data = {
+            'title': 'Updated Title',
+            'description': 'Updated description',
+            'file': uploaded_file  # Setze die Dummy-Datei
+        }
+        response = self.client.put(url, data, format='multipart')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        local_video.refresh_from_db()  # Aktualisiere das lokale Video-Objekt aus der DB
+        self.assertEqual(local_video.title, 'Updated Title')  # Überprüfe, ob der Titel aktualisiert wurde
+
+    def test_delete_local_video(self):
+        # Erstelle ein lokales Video
+        uploaded_file = create_dummy_file(file_name='local_video.mp4')
+        local_video = LocalVideo.objects.create(title='Video to delete', description='Test video', uploaded_by=self.user, file=uploaded_file)
+
+        # Teste das Löschen des lokalen Videos
+        url = reverse('localvideo-detail', args=[local_video.id])  # URL für die Detailansicht
+        response = self.client.delete(url)
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(LocalVideo.objects.count(), 0)  # Überprüfe, ob das Video gelöscht wurde
