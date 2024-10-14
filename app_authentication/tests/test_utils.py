a765a6e05f496261537b837f6f9423683b4f4621
Zwogli <mathiaskohler@web.de>
2024-10-14 04:50:23 +0200

    Refactor tests

--- app_authentication/tests/test_utils.py
+++ app_authentication/tests/test_utils.py
@@ -4,8 +4,8 @@ from django.contrib.auth.tokens import default_token_generator
 from django.utils.http import urlsafe_base64_encode
 from django.utils.encoding import force_bytes
 import json
-from .models import CustomUser
-from .utils import (
+from ..models import CustomUser
+from ..utils import (
     send_verification_email,
     send_reset_password_email,
     parse_request_body,
