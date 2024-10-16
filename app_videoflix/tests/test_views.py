aab193d227789223f358fcdbfa6dcda939b7c4c2
Zwogli <mathiaskohler@web.de>
2024-10-16 08:08:47 +0200

    Fix test update local video

--- app_videoflix/tests/test_views.py
+++ app_videoflix/tests/test_views.py
@@ -1,6 +1,6 @@
 from django.core.files.uploadedfile import InMemoryUploadedFile
 from rest_framework import status
-from rest_framework.test import APITestCase
+from rest_framework.test import APITestCase, APIClient
 from ..models import GlobalVideo, LocalVideo
 from app_authentication.models import CustomUser
 import io
@@ -55,47 +55,51 @@ class LocalVideoViewSetTests(APITestCase):
 
 
     def test_get_local_videos(self):
-        # Erstelle ein lokales Video
         uploaded_file = create_dummy_file(file_name='local_video.mp4')
         LocalVideo.objects.create(title='Local Video', description='Test video', uploaded_by=self.user, file=uploaded_file)
 
-        # Teste das Abrufen der lokalen Videos
-        url = reverse('localvideo-list')  # Name für die Listenansicht
+        url = reverse('localvideo-list')
         response = self.client.get(url)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 1)  # Überprüfe, ob das Video abgerufen wird
+        self.assertEqual(len(response.data), 1)
         self.assertEqual(response.data[0]['title'], 'Local Video')
 
 
     def test_update_local_video(self):
-        # Erstelle ein lokales Video
-        uploaded_file = create_dummy_file(file_name='local_video.mp4')
-        local_video = LocalVideo.objects.create(title='Old Title', description='Test video', uploaded_by=self.user, file=uploaded_file)
+        client = APIClient()
+        client.force_authenticate(user=self.user)
 
-        # Teste das Aktualisieren des lokalen Videos
-        url = reverse('localvideo-detail', args=[local_video.id])  # URL für die Detailansicht
+        uploaded_file = create_dummy_file(file_name='local_video.mp4')
+        local_video = LocalVideo.objects.create(
+            title='Old Title', 
+            description='Test video', 
+            uploaded_by=self.user, 
+            file=uploaded_file
+        )
+
+        # Update only title and discription
+        url = reverse('localvideo-detail', args=[local_video.id])
         data = {
             'title': 'Updated Title',
             'description': 'Updated description',
-            'file': uploaded_file,  # Setze die Dummy-Datei
-            'uploaded_by': self.user.id
         }
-        response = self.client.put(url, data, format='multipart')
+
+        response = client.patch(url, data, format='json')  # For text-updates use JSON-format
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        local_video.refresh_from_db()  # Aktualisiere das lokale Video-Objekt aus der DB
-        self.assertEqual(local_video.title, 'Updated Title')  # Überprüfe, ob der Titel aktualisiert wurde
+
+        local_video.refresh_from_db()
+        self.assertEqual(local_video.title, 'Updated Title')
+        self.assertEqual(local_video.description, 'Updated description')
 
 
     def test_delete_local_video(self):
-        # Erstelle ein lokales Video
         uploaded_file = create_dummy_file(file_name='local_video.mp4')
         local_video = LocalVideo.objects.create(title='Video to delete', description='Test video', uploaded_by=self.user, file=uploaded_file)
 
-        # Teste das Löschen des lokalen Videos
-        url = reverse('localvideo-detail', args=[local_video.id])  # URL für die Detailansicht
+        url = reverse('localvideo-detail', args=[local_video.id])
         response = self.client.delete(url)
 
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-        self.assertEqual(LocalVideo.objects.count(), 0)  # Überprüfe, ob das Video gelöscht wurde
+        self.assertEqual(LocalVideo.objects.count(), 0)
