63e110bb7c8a49ddaeebdb664c2c02c27f348497
Zwogli <mathiaskohler@web.de>
2024-10-24 09:20:25 +0200

    Test test-email

--- app_authentication/tests/test_mail.py
+++ app_authentication/tests/test_mail.py
@@ -1,23 +1,23 @@
-from django.conf import settings
-from rest_framework import status
-from rest_framework.response import Response
-from django.core.mail import BadHeaderError, send_mail
+# from django.conf import settings
+# from rest_framework import status
+# from rest_framework.response import Response
+# from django.core.mail import BadHeaderError, send_mail
 
 
-def send_test_email(request):
-    """
-    Sends a test email to the specified recipient.
-    """
-    try:
-        send_mail(
-            'Test E-Mail',
-            'Dies ist eine Test-E-Mail.',
-            settings.DEFAULT_FROM_EMAIL,
-            ['mathiaskohler@mail.de'],
-            fail_silently=False,
-        )
-        return Response({'message': 'Test email has been sent.'}, status=status.HTTP_200_OK)
-    except BadHeaderError:
-        return Response({'error': 'Invalid header found.'}, status=status.HTTP_400_BAD_REQUEST)
-    except Exception as e:
-        return Response({'error': f'Error when sending the e-mail: {str(e)}'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+# def send_test_email(request):
+#     """
+#     Sends a test email to the specified recipient.
+#     """
+#     try:
+#         send_mail(
+#             'Test E-Mail',
+#             'Dies ist eine Test-E-Mail.',
+#             settings.DEFAULT_FROM_EMAIL,
+#             ['mathiaskohler@mail.de'],
+#             fail_silently=False,
+#         )
+#         return Response({'message': 'Test email has been sent.'}, status=status.HTTP_200_OK)
+#     except BadHeaderError:
+#         return Response({'error': 'Invalid header found.'}, status=status.HTTP_400_BAD_REQUEST)
+#     except Exception as e:
+#         return Response({'error': f'Error when sending the e-mail: {str(e)}'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
