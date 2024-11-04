32871e7822f27b2c1114a1a3960bac4dc24fd674
Zwogli <mathiaskohler@web.de>
2024-11-04 17:33:51 +0100

    Fix ssh connection

--- up.bat
+++ up.bat
@@ -2,8 +2,8 @@ git pull
 git add .
 git commit -m "%*"
 git push
-ssh zwogli\zwogli@35.234.78.80 "cd /home/mathiaskohler91/projects/videoflix-backend/ && git pull"
-ssh zwogli\zwogli@35.234.78.80 "sudo supervisorctl restart videoflix_gunicorn"
-ssh zwogli\zwogli@35.234.78.80 "sudo supervisorctl restart videoflix-rq-worker"
-ssh zwogli\zwogli@35.234.78.80 "sudo /usr/sbin/nginx -s reload"
+ssh mathi@35.234.78.80 "cd /home/mathiaskohler91/projects/videoflix-backend/ && git pull"
+ssh mathi@35.234.78.80 "sudo supervisorctl restart videoflix_gunicorn"
+ssh mathi@35.234.78.80 "sudo supervisorctl restart videoflix-rq-worker"
+ssh mathi@35.234.78.80 "sudo /usr/sbin/nginx -s reload"
 
