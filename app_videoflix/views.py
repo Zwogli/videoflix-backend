25d862f49f887ac1e85d9c4c974a81f07a251a7a
Zwogli <mathiaskohler@web.de>
2024-11-05 14:04:59 +0100

    Test up server git pull

--- app_videoflix/views.py
+++ app_videoflix/views.py
@@ -93,7 +93,6 @@ def thumbnail_status(request, video_id):
     except LocalVideo.DoesNotExist:
         return Response({'error': 'Video not found'}, status=status.HTTP_404_NOT_FOUND)
 
-
 # @api_view(['GET'])
 # @permission_classes([AllowAny])
 # def thumbnail_status(request, video_id):
