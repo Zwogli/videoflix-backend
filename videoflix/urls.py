1c2d139577426b49418dd0ea2a3d50f6f0eaa10f
Zwogli <mathiaskohler@web.de>
2024-11-18 15:46:25 +0100

    Add url redirect

--- videoflix/urls.py
+++ videoflix/urls.py
@@ -20,9 +20,11 @@ from django.urls import path, include
 from django.conf.urls.static import static
 import debug_toolbar
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from .views import redirect_to_admin
 
 
 urlpatterns = [
+    path('', redirect_to_admin), 
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')), # Django-allauth for user authentication and registration
     path('auth/', include('app_authentication.urls')),  # Custom authentication app
