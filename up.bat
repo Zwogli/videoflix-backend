2565c13c4971c3d84b3fde92cc0c44c2248aa840
Zwogli <mathiaskohler@web.de>
2024-12-02 08:36:10 +0100

    Update ip-adresse

--- up.bat
+++ up.bat
@@ -2,8 +2,7 @@ git pull
 git add .
 git commit -m "%*"
 git push
-ssh mathi@35.234.78.80 "cd /home/mathiaskohler91/projects/videoflix-backend/ && git pull"
-ssh mathi@35.234.78.80 "sudo supervisorctl restart videoflix_gunicorn"
-ssh mathi@35.234.78.80 "sudo supervisorctl restart videoflix-rq-worker"
-ssh mathi@35.234.78.80 "sudo /usr/sbin/nginx -s reload"
-
+ssh mathi@34.159.49.18 "cd /home/mathiaskohler91/projects/videoflix-backend/ && git pull"
+ssh mathi@34.159.49.18 "sudo supervisorctl restart videoflix_gunicorn"
+ssh mathi@34.159.49.18 "sudo supervisorctl restart videoflix-rq-worker"
+ssh mathi@34.159.49.18 "sudo /usr/sbin/nginx -s reload"
